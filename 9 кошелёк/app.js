b1337260803f1c9373aecdaa6237f3f76495b72f
Panarin Anatoliy <Tolik_chapaev@mail.ru>
2024-10-28 20:54:44 +0300

    new home work

---
+++ 9 кошелёк/app.js
@@ -0,0 +1,36 @@
+const wallet = {
+    balance: 0,
+    operations: [],
+    increase: function(sum, reason) {
+        this.balance += sum;
+        this.operations.push({
+            reason: reason,
+            sum: sum
+        })
+        return true;
+    },
+    decrease: function(sum, reason) {
+        if (this.balance < sum) {
+            console.log('Недостаточно баланса');
+            return false;
+        }
+        this.balance -= sum;
+        this.operations.push({
+            reason: reason,
+            sum: -sum
+        })
+        return true;
+    },
+    getOperationLenght: function () {
+        return this.operations.length;
+    }
+};
+
+console.log(wallet.increase(1000, 'Зарплата'));
+console.log(wallet.getOperationLenght());
+console.log(wallet.decrease(2000, 'Покупка ноутбука'));
+console.log(wallet.getOperationLenght());
+console.log(wallet.decrease(500, 'Покупка телефона'));
+console.log(wallet.getOperationLenght());
+console.log(wallet.balance);
+console.log(wallet.operations);

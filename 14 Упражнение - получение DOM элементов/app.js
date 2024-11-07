2dc5f938ddfb5b4960b381ef77c3bda111985392
Panarin Anatoliy <Tolik_chapaev@mail.ru>
2024-11-07 19:36:29 +0300

    DOM

---
+++ 14 Упражнение - получение DOM элементов/app.js
@@ -0,0 +1,25 @@
+'use strict';
+
+function submitForm(){
+    const input = document.querySelector('.input').value;
+    if (!input) {
+        return;
+    }
+    document.querySelector('.panel').innerText = input;
+    document.querySelector('.input').value = ' ';
+    document.querySelector('.notification').classList.remove('notification_hidden')
+}
+
+function inputChanged(e) {
+    if (e.code == 'Enter') {
+        submitForm()
+    }
+}
+
+// console.log(document.querySelector('.one').innerText);
+// console.log(document.querySelector('.one ~ div').innerText);
+console.log(document.querySelectorAll('.one')[0].innerText);
+console.log(document.querySelectorAll('.one')[1].innerText);
+// console.log(document.querySelector('#two').innerText);
+console.log(document.getElementById('two').innerText);
+console.log(document.querySelector('[user-id="4"]').innerText);
